cf25c8d2e5015b3503d94cc3182633249ab47201
adityap5 <aditya29pippal@gmail.com>
2023-06-28 20:08:58 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
+// import About from "./components/About";
 import './App.css';
+import {useState} from "react";
 function App() {
+
+const[mode,setMode] =useState('light');
+const toggleMode=()=>{
+  if(mode ==='light'){
+    setMode('dark');
+    document.body.style.backgroundColor= 'grey';
+  }else{
+    setMode('light');
+    document.body.style.backgroundColor= 'white';
+
+  }
+}
+
   return (
    <>
-   
-   
-   <Navbar title="textChanger" about="About"/>
-   <div className="container  my-3">
-<Textform heading="Enter your text to Analyze"/>
 
+   <Navbar title="textChanger" about="About" mode={mode} toggleMode={toggleMode}/>
+   <div className="container  my-3">
+<Textform heading="Enter your text to Analyze"mode={mode}/>
+{/* <About/> */}
 {/* <button className="btn btn-primary">Convert to UpperCase</button> 
 <button className="btn btn-primary">Convert to LowerCase</button> */}
 </div>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,49 +1,56 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Textform(props) {
-    const handleUpClick=()=>{
-        
-        let newtext= text.toUpperCase();
-        setText(newtext)
-    }
-    const handleDownClick=()=>{
-       
-        let newtext= text.toLowerCase();
-        setText(newtext)
-    }
-    const handleOnChange=(event)=>{
+  const handleUpClick = () => {
 
-setText(event.target.value)
-    }
-    const handleclearClick=()=>{
-        let newtext="";
-        setText(newtext)
-    }
-    const[text, setText] =useState('');
+    let newtext = text.toUpperCase();
+    setText(newtext)
+  }
+  const handleDownClick = () => {
+
+    let newtext = text.toLowerCase();
+    setText(newtext)
+  }
+  const handleOnChange = (event) => {
+
+    setText(event.target.value)
+  }
+  const handleclearClick = () => {
+    let newtext = "";
+    setText(newtext)
+  }
+  const handleOnCopy = () => {
+    var text = document.getElementById("myBox")
+    text.select();
+    navigator.clipboard.writeText(text.value);
+
+  }
+  const [text, setText] = useState('');
   return (
 
     <>
-      <div>
-        <h2>{props.heading}</h2>
-< div className="mb-3">
-  
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="9"></textarea>
-</div>
+      <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+        <h2 >{props.heading}</h2>
+        < div className="mb-3">
 
-<button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
-<button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to Lowercase</button>
-<button className="btn btn-primary mx-2" onClick={handleclearClick}>Clear Text</button>
+          <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="9"></textarea>
+        </div>
+
+        <button className="btn btn-dark my-3" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-dark mx-2" onClick={handleDownClick}>Convert to Lowercase</button>
+        <button className="btn btn-dark mx-2" onClick={handleclearClick}>Clear Text</button>
+        <button className="btn btn-dark mx-2" onClick={handleOnCopy}>Copy Text</button>
 
 
-</div>
-<div className="container my-4">
-<h3>Your Text Summary</h3>
-<p>{text.split(" ").length} words {text.length}characters</p>
-<h3>Preview</h3>
-<p>{text}</p>
+      </div>
+      <div className="container my-4" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+        <h3>Your Text Summary</h3>
+        <p>{text.split(" ").length} words {text.length}characters</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
 
-</div>
-   
+      </div>
+
     </>
   )
 }
